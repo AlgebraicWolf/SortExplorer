f8e116664d69528514d16bde12787dabef5c1371
Alexey Volkov <volkov.aa@phystech.edu>
2020-09-10 00:08:24 +0300

    Implemented window creation

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-#include "sortstat.hpp"
 #include "sorts.hpp"
+#include "sortstat.hpp"
 
 int main() {
-    int arr0[] = {5, 6, 2, 1, 4, 3, 8, 0, 9};
-    SortingStat<int> arr[9];
-    for(int i = 0; i < 9; i++) {
-        arr[i] = arr0[i];
+    sf::RenderWindow mainWindow(sf::VideoMode(800, 600), "Sorting algorithm analyzer", sf::Style::None);
+    mainWindow.clear();
+    mainWindow.display();
+
+    while (mainWindow.isOpen()) {
+        sf::Event event;
+        
+        while (mainWindow.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    mainWindow.close();
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
-    SortingStat<int>::resetCounters();
-    QuickSort<int> b;
-    b(arr0, 9);
-    
-    5 == 6;
 
-    for(int i = 0; i < 9; i++)
-        std::cout << arr0[i] << " ";
-
-    std::cout << "\nAssignments: " << SortingStat<int>::getAssignmentCount() << "\nComparisons: " << SortingStat<int>::getComparisonCount() << "\n";
+    return 0;
 }
